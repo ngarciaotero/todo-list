faea52e9967985f7e68cda68e54f6f9eea2e8a1d
ngarciaotero <nasleygarciaotero@gmail.com>
2024-05-18 12:32:06 -0400

    Refactor the way project data is stored and retrieved from localStorage and creation of project related objects
    Refactor initializeDefaultProjects function
    
    - instead of storing instances of objects in localStorage dataToStore
    creates a new array of objects containing only the necessary data properties
    in order to recreate the instances later.
    
    Call dataToStore on load
    
    Update image paths in json file

--- src/services/projectLocalStorage.js
+++ src/services/projectLocalStorage.js
@@ -1,40 +1,24 @@
-import projectList from "../models/projectList.js";
-import project from "../models/project.js";
-import todoList from "../models/todoList.js";
-import todoItem from "../models/todoItem.js";
 import {
   getRandomDateString,
   setRandomItemStatus,
 } from "../utils/setRandomParameters.js";
 import defaultProjectData from "../data/defaultProjectData.json";
 
-const initializeDefaultProjects = () => {
-  // check if there is already data stored in localStorage
-  const existingProjectList = JSON.parse(localStorage.getItem("projectList"));
-  const projectListInstance = existingProjectList || projectList();
+const dataToStore = defaultProjectData.map((projectData) => {
+  const todoItems = projectData.todoItems.map((todoItemData) => ({
+    title: todoItemData.title,
+    description: todoItemData.description,
+    dueDate: getRandomDateString(todoItemData.daysRange),
+    priority: todoItemData.priority,
+    status: setRandomItemStatus(),
+  }));
+  return {
+    name: projectData.name,
+    path: projectData.path,
+    todoItems,
+  };
+});
 
-  if (!existingProjectList) {
-    defaultProjectData.forEach((projectData) => {
-      const projectTodoList = todoList();
-      projectData.todoItems.forEach((todoItemData) => {
-        const newTodoItem = todoItem(
-          todoItemData.title,
-          todoItemData.description,
-          getRandomDateString(todoItemData.daysRange),
-          todoItemData.priority
-        );
-        setRandomItemStatus(newTodoItem);
-        projectTodoList.addTodoItem(newTodoItem);
-      });
-      const newProject = project(
-        projectData.name,
-        projectData.path,
-        projectTodoList
-      );
-      projectListInstance.addProject(newProject);
-    });
-    localStorage.setItem("projectList", JSON.stringify(projectListInstance));
-  }
-};
+localStorage.setItem("projectList", JSON.stringify(dataToStore));
 
-export default initializeDefaultProjects;
+export default dataToStore;
