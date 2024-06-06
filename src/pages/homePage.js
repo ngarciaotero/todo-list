c7eba84470e6e6394b1262ea334ffb01f7f3fb29
ngarciaotero <nasleygarciaotero@gmail.com>
2024-06-06 14:19:02 -0400

    Creates the home page display
    - updates the header and img header
    - the content container will consist of the home page dashboard which
    is created by called the createHomePageDashboard function

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,3 +1,20 @@
-const createHomePage = () => {};
+import {
+  createPageHeader,
+  createProjectPageHeaderImg,
+} from "../ui/createTitle.js";
+import { createHomePageDashboard } from "../ui/createContent.js";
+
+const createHomePage = () => {
+  const contentContainer = document.querySelector("#content");
+  const titleContainer = document.querySelector(".page-title");
+  const dashboardContainer = document.createElement("div");
+
+  dashboardContainer.appendChild(createHomePageDashboard());
+  titleContainer.appendChild(createPageHeader("Home"));
+  titleContainer.appendChild(
+    createProjectPageHeaderImg("kitten-scribbles.png")
+  );
+  contentContainer.appendChild(dashboardContainer);
+};
 
 export default createHomePage;
