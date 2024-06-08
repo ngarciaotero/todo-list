4436e3aed2fcd3cd9071d6cc80ada3a9fedd6d99
ngarciaotero <nasleygarciaotero@gmail.com>
2024-06-08 02:34:02 -0400

    Add functionality to filter tasks by due date conditions
    - getTasksByDueDateCondition filters tasks based on a specified date condition
    - sortTasksByDueDate access the correct task object and sorts tasks by their due date
    - getDueSoonTasks returns tasks that are within the specified threshold
    - getOverdueTasks returns tasks that are overdue

--- src/models/projectList.js
+++ src/models/projectList.js
@@ -1,3 +1,6 @@
+import { differenceInDays } from "date-fns";
+import { sortByDueDate } from "../helpers/sorting.js";
+
 const projectList = () => ({
   projectList: [],
 
@@ -34,6 +37,43 @@ const projectList = () => ({
       completedTasks += todoList.getCompletedFilteredList().length;
     });
     return completedTasks;
+  },
+
+  getTasksByDueDateCondition(condition, daysThreshold = 30) {
+    const currentDate = new Date();
+    const filteredTasks = [];
+
+    this.projectList.forEach((project) => {
+      const todoList = project.getTodoList();
+      const notStartedTasks = todoList.getNotStartedFilteredList();
+
+      const tasksMatchingCondition = notStartedTasks.filter((task) => {
+        const dueDate = task.getDueDate();
+        const daysDiff = differenceInDays(dueDate, currentDate);
+        return condition(daysDiff, daysThreshold);
+      });
+
+      tasksMatchingCondition.forEach((task) => {
+        filteredTasks.push({ task, project });
+      });
+    });
+    filteredTasks.sort(this.sortTasksByDueDate);
+    return filteredTasks;
+  },
+
+  getDueSoonTasks(daysThreshold = 30) {
+    return this.getTasksByDueDateCondition(
+      (daysDiff, threshold) => daysDiff <= threshold && daysDiff >= 0,
+      daysThreshold
+    );
+  },
+
+  getOverdueTasks() {
+    return this.getTasksByDueDateCondition((daysDiff) => daysDiff < 0);
+  },
+
+  sortTasksByDueDate(a, b) {
+    return sortByDueDate(a.task, b.task);
   },
 });
 
